8a6c64143c2fcb17c1f9a07af188f7313d73c9aa
LucasButto <lbutto40@gmail.com>
2022-09-11 19:31:52 -0300

    Pagina completa

--- js/common.js
+++ js/common.js
@@ -1,57 +1,42 @@
 function getCitiesFromLocalStorage() {
-    let cities = localStorage.getItem("CITIES");
+  let cities = localStorage.getItem("CITIES");
 
-    if (cities) {
-        cities = JSON.parse(cities);
-    } else {
-        cities = [];
-    }
-    return cities;
+  if (cities) {
+    cities = JSON.parse(cities);
+  } else {
+    cities = [];
+  }
+  return cities;
 }
 
 function buscarDatos(cityName) {
-    let apiKey = "2c405c01826f37c50c9e4ef65e6e442d"
-    return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric&lang=es`)
-        .then(response => {
-            if (response.ok) {
-                sacarSpinner();
-                return response.json();
-            }
-            throw new Error("error")
-        })
-        .then(data => {
-            mostrarTarjeta(data);
-        })
-        .catch(error => {
-            return "error"
-        });
-}
+  let apiKey = "2c405c01826f37c50c9e4ef65e6e442d";
+  const data = fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric&lang=es`
+  )
+    .then((response) => {
+      if (response.ok) {
+        sacarSpinner();
+        return response.json();
+      }
+      throw new Error("error");
+    })
+    .then((data) => {
+      mostrarTarjeta(data);
+    })
+    .catch((error) => {
+      return "error";
+    });
 
-function mostrarTarjeta(data) {
-
-    if (document.getElementById("section-weather-result")) {
-        document.getElementById("section-weather-result").innerHTML = '';
-        document.getElementById("section-weather-result").innerHTML = 
-                            `<div class="card">
-                                <h3>${data.name}</h3>
-                                <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="Imagen del clima">
-                                <p>Temperatura: ${data.main.temp}°</p>
-                                <p>Sensación Térmica: ${data.main.feels_like}°</p>
-                                <p>Humedad: ${data.main.humidity}%</p>
-                                <p>Velocidad del Viento: ${data.wind.speed} km/h</p>
-                                <p>Presión: ${data.main.pressure} P</p>
-                            </div>`;
-    }
+  return data;
 }
 
 function sacarMensaje() {
-    setTimeout(function() {
-        document.getElementsByClassName("mensajes")[0].remove();
-    }, 5000);
+  setTimeout(function () {
+    document.getElementsByClassName("mensajes")[0].remove();
+  }, 5000);
 }
 
 function sacarSpinner() {
-    
-    document.querySelector('.spinner').style.display = 'none';
-    
+  document.querySelector(".spinner").style.display = "none";
 }
--- js/pages/index.js
+++ js/pages/index.js
@@ -1,28 +1,43 @@
 let selector = document.getElementById("city-list");
 
 function agregarCiudadesSelector() {
-    let cities = getCitiesFromLocalStorage();
+  let cities = getCitiesFromLocalStorage();
 
-    if (cities === []) {
-        selector.innerHTML += `<option value="noCities" disabled selected>No hay ciudades agregadas</option>`
+  if (cities === []) {
+    selector.innerHTML += `<option value="noCities" disabled selected>No hay ciudades agregadas</option>`;
+  } else {
+    selector.innerHTML += `<option value="" disabled selected>Seleccionar Ciudad</option>`;
+    for (let i = 0; i < cities.length; i++) {
+      selector.innerHTML += `<option value="${cities[i]}">${cities[i]}</option>`;
     }
-    else {
-        selector.innerHTML += `<option value="" disabled selected>Seleccionar Ciudad</option>`
-        for (let i = 0; i < cities.length; i++) {
-            selector.innerHTML += `<option value="${cities[i]}">${cities[i]}</option>`
-        }
-    }
+  }
 }
 
 function crearTarjeta() {
+  document.querySelector(".spinner").style.display = "block";
 
-    document.querySelector('.spinner').style.display = 'block';
-    
-    buscarDatos(selector.value);
-    
-    document.querySelector('.card').style.display = 'none';
+  buscarDatos(selector.value);
+
+  document.querySelector(".card").style.display = "none";
 }
 
-document.getElementById("consultar").addEventListener("click", crearTarjeta)
+function mostrarTarjeta(data) {
+  if (document.getElementById("section-weather-result")) {
+    document.getElementById("section-weather-result").innerHTML = "";
+    document.getElementById(
+      "section-weather-result"
+    ).innerHTML = `<div class="card">
+                                <h3>${data.name}</h3>
+                                <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="Imagen del clima">
+                                <p>Temperatura: ${data.main.temp}°</p>
+                                <p>Sensación Térmica: ${data.main.feels_like}°</p>
+                                <p>Humedad: ${data.main.humidity}%</p>
+                                <p>Velocidad del Viento: ${data.wind.speed} km/h</p>
+                                <p>Presión: ${data.main.pressure} P</p>
+                            </div>`;
+  }
+}
+
+document.getElementById("consultar").addEventListener("click", crearTarjeta);
 
 agregarCiudadesSelector();
